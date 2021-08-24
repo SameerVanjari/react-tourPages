6d491254c884efa68c86ff680cd02c1a45420e8c
Sameer <ssv6132@gmail.com>
2021-08-24 16:11:12 +0530

    Updates

---
+++ precache-manifest.023872fb1ba35582670e94ac24c7a9b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d0beab1484cd84e71700e9d3d83fcf",
+    "url": "/react-tourPages/index.html"
+  },
+  {
+    "revision": "fe34eb198468d8e281da",
+    "url": "/react-tourPages/static/css/main.c3e53c0d.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/react-tourPages/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tourPages/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe34eb198468d8e281da",
+    "url": "/react-tourPages/static/js/main.40ebc9d9.chunk.js"
+  },
+  {
+    "revision": "1d760fdd950bf4ad9ba7",
+    "url": "/react-tourPages/static/js/runtime-main.e38424ec.js"
+  }
+]);
